af891584e4238c0182f8061d5e458041316a701b
D'Artagnan von Pingel <dvonpingel@gmail.com>
2021-10-23 00:23:42 -0600

    Adds Searchbar

--- src/ChampionList.js
+++ src/ChampionList.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
 import Champion from './Champion'
 import champArray from './champArray'
+import Searchbar from './SearchBar'
+import { useDebouncedState } from './hooks/useDebouncedState'
 
 const ChampList = (props) => {
   const { isLoading } = props
+  const [search, setSearch] = useState(''),
+    debouncedSearch = useDebouncedState(search, 300)
 
   if (isLoading) {
     return <h1>Loading Chapmion...</h1>
@@ -12,9 +15,14 @@ const ChampList = (props) => {
 
   return (
     <div className='champListContainer'>
-      {champArray.map((champ, index) => (
-        <Champion key={index} champ={champ} />
-      ))}
+      <Searchbar search={search} setSearch={setSearch} />
+      {champArray
+        .filter((champ) =>
+          champ.name.toLowerCase().includes(debouncedSearch?.toLowerCase())
+        )
+        .map((champ, index) => (
+          <Champion key={index} champ={champ} />
+        ))}
     </div>
   )
 }
