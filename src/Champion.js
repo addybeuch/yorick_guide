1126c9ea7019fdbce98872f8bc417162eb4465c7
addybeuch <addybeuch@gmail.com>
2021-10-23 00:41:38 -0600

    Changed Champion to use comment and matchup, background image rendered

--- src/Champion.js
+++ src/Champion.js
@@ -6,10 +6,11 @@ const Champion = (props) => {
   return (
     <div className='card'>
       <h3 className='name'>{champ.name}</h3>
-      <p className='counter'>{champ.counter}</p>
+      <p className='matchup'>{champ.matchup}</p>
       <p className='runes'>{champ.runes}</p>
       <p className='items'>{champ.items}</p>
       <p className='difficulty'>{champ.difficulty}</p>
+      <p className='comment'>{champ.comment}</p>
     </div>
   )
 }
