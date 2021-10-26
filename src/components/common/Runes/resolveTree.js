f6c8d08788395baa75059ac8bf83587f95fb2cb8
D'Artagnan von Pingel <dvonpingel@gmail.com>
2021-10-25 22:03:33 -0600

    adds nav bar slightly and styles runes

--- src/components/common/Runes/resolveTree.js
+++ src/components/common/Runes/resolveTree.js
@@ -4,23 +4,27 @@ import runeList from './runeImages/runeList'
 function Resolve() {
   return (
     <div className='resolveTree'>
-      <img src={runeList.resolveIcon} alt='' />
-      <div className='resolveRunes'>
-        <div className='bigThree'>
-          <img src={runeList.graspOfTheUndying} alt='' />
-          <img src={runeList.aftershock} alt='' />
-          <img src={runeList.guardian} alt='' />
-        </div>
-        <div className='smallRunes'>
+      <img className='resolve-img' src={runeList.resolveIcon} alt='' />
+      <div className='bigThree'>
+        <img src={runeList.graspOfTheUndying} alt='' />
+        <img className='greyed-out' src={runeList.aftershock} alt='' />
+        <img className='greyed-out' src={runeList.guardian} alt='' />
+      </div>
+      <div className='smallRunes'>
+        <div className='triple-stack'>
           <img src={runeList.demolish} alt='' />
-          <img src={runeList.fontOfLife} alt='' />
-          <img src={runeList.sheildBash} alt='' />
+          <img className='greyed-out' src={runeList.fontOfLife} alt='' />
+          <img className='greyed-out' src={runeList.sheildBash} alt='' />
+        </div>
+        <div className='triple-stack'>
           <img src={runeList.conditioning} alt='' />
-          <img src={runeList.secondWind} alt='' />
-          <img src={runeList.bonePlating} alt='' />
+          <img className='greyed-out' src={runeList.secondWind} alt='' />
+          <img className='greyed-out' src={runeList.bonePlating} alt='' />
+        </div>
+        <div className='triple-stack'>
           <img src={runeList.overgrowth} alt='' />
-          <img src={runeList.revitalize} alt='' />
-          <img src={runeList.unflinching} alt='' />
+          <img className='greyed-out' src={runeList.revitalize} alt='' />
+          <img className='greyed-out' src={runeList.unflinching} alt='' />
         </div>
       </div>
     </div>
