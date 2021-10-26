83eff8fe7d6f3014e074fac360a8188dd49ebeae
addybeuch <addybeuch@gmail.com>
2021-10-25 21:32:40 -0600

    Images imported and rendered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import ChampList from './components/ChampionList'
 import Item from './components/common/Item/Item'
+import Resolve from './components/common/Runes/resolveTree'
 
 function App() {
   return (
     <div className='guide'>
       <h1>Yorick Guide</h1>
       <Item name='hullBreaker' />
+      <Resolve />
       <ChampList />
     </div>
   )
---
+++ src/components/common/Runes/runeImages/runeList.js
@@ -0,0 +1,31 @@
+import aftershock from './Aftershock_rune.png'
+import conditioning from './Conditioning_rune.png'
+import demolish from './Demolish_rune.png'
+import fontOfLife from './Font_of_Life_rune.png'
+import graspOfTheUndying from './Grasp_of_the_Undying_rune.png'
+import guardian from './Guardian_rune.png'
+import overgrowth from './Overgrowth_rune.png'
+import resolveIcon from './Resolve_icon.png'
+import revitalize from './Revitalize_rune.png'
+import secondWind from './Second_Wind_rune.png'
+import sheildBash from './Shield_Bash_rune.png'
+import unflinching from './Unflinching_rune.png'
+import bonePlating from './Bone_Plating_rune.png'
+
+const resolveRunesList = {
+  aftershock: aftershock,
+  conditioning: conditioning,
+  demolish: demolish,
+  fontOfLife: fontOfLife,
+  graspOfTheUndying: graspOfTheUndying,
+  guardian: guardian,
+  overgrowth: overgrowth,
+  resolveIcon: resolveIcon,
+  revitalize: revitalize,
+  secondWind: secondWind,
+  sheildBash: sheildBash,
+  unflinching: unflinching,
+  bonePlating: bonePlating,
+}
+
+export default resolveRunesList
